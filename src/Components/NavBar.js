e728111e92743954b663ddf3807bf9ebb3f03b3b
David Hu <y467hu@uwaterloo.ca>
2021-09-18 00:27:48 -0400

    NavBar Improvements

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,20 +3,69 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import { deepOrange } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Logo from "../Images/Eventlink-logo.png";
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#DECEA1",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+  },
+});
+
+const toolBarItemTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#3D3D3D",
+    },
+
+    secondary: {
+      main: "#000000",
+    },
+  },
+});
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Event Link
-          </Typography>
-          <Button color="inherit">
-            <img src="" alt="" />
-          </Button>
-        </Toolbar>
-      </AppBar>
+      <ThemeProvider theme={customTheme}>
+        <AppBar position="static" color="primary">
+          <ThemeProvider theme={toolBarItemTheme}>
+            <Toolbar>
+              {/* <img src={Logo} alt="EventLink Logo" /> */}
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1 }}
+                color="primary"
+              >
+                Event Link
+              </Typography>
+              {/* <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              > */}
+              <Button variant="text" color="primary">
+                Host
+              </Button>
+              {/* </IconButton> */}
+              <Button color="inherit">
+                <Avatar sx={{ bgcolor: deepOrange[500] }}>NL</Avatar>
+              </Button>
+            </Toolbar>
+          </ThemeProvider>
+        </AppBar>
+      </ThemeProvider>
     </Box>
   );
 }
